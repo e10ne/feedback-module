ed2e9bc25792ceb2d2b81ad4bffea172e1fbea91
e10ne <etiennevanhalteren@hotmail.com>
2022-12-21 11:58:33 +0100

    Update index to have placeholder text and button link to create-feedback page

---
+++ src/pages/create-feedback.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@chakra-ui/react";
+import Layout from "../components/layout/Layout";
+
+const CreateFeedback: React.FC<{}> = ({}) => {
+  return (
+    <Layout>
+      <Text>Hello</Text>
+    </Layout>
+  );
+};
+
+export default CreateFeedback;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,38 @@
-import { Text, Code } from "@chakra-ui/react";
+import { Button, Flex, Text } from "@chakra-ui/react";
 import Layout from "../components/layout/Layout";
+import NextLink from "next/link";
 
-const Index = () => (
-  <Layout>
-    <Text color="text">
-      Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{" "}
-      <Code>TypeScript</Code>.
-    </Text>
-  </Layout>
-);
+const Index = ({}) => {
+  return (
+    <Layout>
+      <Text color="text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Text>
+      <Flex
+        justify={"center"}
+        mt={"20"}
+      >
+        <NextLink href={"/create-feedback"}>
+          <Button
+            bgColor={"button"}
+            alignSelf={"flex-end"}
+            fontSize={"36px"}
+            size={"lg"}
+            px={"14"}
+            py={"10"}
+          >
+            Geef Feedback
+          </Button>
+        </NextLink>
+      </Flex>
+    </Layout>
+  );
+};
 
 export default Index;

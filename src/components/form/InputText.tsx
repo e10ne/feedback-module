e023e42e29d17725a82310757f606de435bf377a
e10ne <etiennevanhalteren@hotmail.com>
2023-01-13 12:08:33 +0100

    Create input component

---
+++ src/components/form/InputText.tsx
@@ -0,0 +1,40 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import { useField } from "formik";
+import { InputHTMLAttributes } from "react";
+
+type InputTextProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  size?: string | undefined;
+  label: string;
+};
+
+const InputText: React.FC<InputTextProps> = ({ label, ...props }) => {
+  const [field, { error }] = useField(props);
+
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel
+        htmlFor={field.name}
+        fontSize={"18px"}
+      >
+        {label}
+      </FormLabel>
+      <Input
+        {...field}
+        {...props}
+        _hover={{ borderColor: "gray.600" }}
+        id={field.name}
+        placeholder={label}
+        borderColor={"gray.400"}
+      />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
+
+export default InputText;

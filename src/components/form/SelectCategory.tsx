bd96e78b74d18a444cfb1766a6507d1608f4b86e
e10ne <etiennevanhalteren@hotmail.com>
2023-01-16 11:25:57 +0100

    Add some styling to SelectCategory to be consistent with the rest of the form

--- src/components/form/SelectCategory.tsx
+++ src/components/form/SelectCategory.tsx
@@ -4,7 +4,11 @@ const SelectCategory: React.FC<{}> = () => {
   return (
     <FormControl>
       <FormLabel fontSize={"18px"}>Categorie*</FormLabel>
-      <Select name="category">
+      <Select
+        name="category"
+        borderColor={"gray.400"}
+        _hover={{ borderColor: "gray.600" }}
+      >
         <option>Placeholder</option>
         <option>another placeholder</option>
       </Select>

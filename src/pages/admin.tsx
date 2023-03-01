1ab8aee92444ef7a83bd802bbf5ece64b63f8a4a
e10ne <etiennevanhalteren@hotmail.com>
2023-03-01 12:04:35 +0100

    Add active feedbacks to admin page

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,23 +1,29 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Flex, Heading, Text } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../../lib/createUrqlClient";
 import Categories from "../components/admin/Categories";
+import Feedbacks from "../components/admin/Feedbacks";
 import Layout from "../components/layout/Layout";
 
 const AdminPage: React.FC<{}> = () => {
   return (
     <Layout>
-      <Heading variant={"pageHeader"}>Feedback beheerderpagina</Heading>
+      <Flex
+        flexDirection={"column"}
+        gap={"4"}
+        pb={"3em"}
+      >
+        <Heading variant={"pageHeader"}>Feedback beheerderpagina</Heading>
 
-      <Text>Searchbar here...</Text>
+        <Text>Searchbar here...</Text>
 
-      <Categories />
+        <Categories />
 
-      <Heading variant={"subHeader"}>Feedback</Heading>
-      <Text>Feedback here...</Text>
+        <Feedbacks />
 
-      <Heading variant={"subHeader"}>Archief feedback</Heading>
-      <Text>Archived feedback here...</Text>
+        <Heading variant={"subHeader"}>Archief feedback</Heading>
+        <Text>Archived feedback here...</Text>
+      </Flex>
     </Layout>
   );
 };

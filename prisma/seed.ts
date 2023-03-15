06edc35d585afb6956b431e48b709cc079dec929
e10ne <etiennevanhalteren@hotmail.com>
2023-03-15 13:30:28 +0100

    Remove medient from seeder && Add categories to seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,7 +3,6 @@ import * as argon2 from "argon2";
 const prisma = new PrismaClient();
 
 const adminPassword = process.env.ADMIN_PASS as string;
-const medientPassword = process.env.MEDIENT_PASS as string;
 
 async function main() {
   await prisma.user.create({
@@ -13,11 +12,12 @@ async function main() {
     },
   });
 
-  await prisma.user.create({
-    data: {
-      username: "medient",
-      password: await argon2.hash(medientPassword),
-    },
+  await prisma.category.createMany({
+    data: [
+      { title: "Styling" },
+      { title: "Network Errors" },
+      { title: "Login Errors" },
+    ],
   });
 }
 

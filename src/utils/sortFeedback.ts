d9cb896089804a7ff340a81b1d59ad147da74977
e10ne <etiennevanhalteren@hotmail.com>
2023-03-21 10:26:13 +0100

    Create sorting util for feedbacks

---
+++ src/utils/sortFeedback.ts
@@ -0,0 +1,48 @@
+import { Feedback } from "../../graphql/generated/graphql";
+
+interface sortFeedbackValues {
+  feedbacks: Feedback[] | [];
+  sortOption: string;
+}
+
+export function sortFeedback(values: sortFeedbackValues): Array<any> {
+  if (values.feedbacks.length <= 0) {
+    return [];
+  }
+
+  let result: any[] = [];
+
+  if (values.sortOption === "") {
+    result = values.feedbacks;
+  } else if (values.sortOption === "dateNew") {
+    result = values.feedbacks.sort((a, b) => {
+      if (a.id > b.id) return -1;
+      if (a.id < b.id) return 1;
+      return 0;
+    });
+  } else if (values.sortOption === "dateOld") {
+    result = values.feedbacks.sort((a, b) => {
+      if (a.id > b.id) return 1;
+      if (a.id < b.id) return -1;
+      return 0;
+    });
+  } else if (values.sortOption === "nameAsc") {
+    result = values.feedbacks.sort((a, b) => {
+      if (a.category!.title!.toLowerCase() < b.category!.title!.toLowerCase())
+        return -1;
+      if (a.category!.title!.toLowerCase() > b.category!.title!.toLowerCase())
+        return 1;
+      return 0;
+    });
+  } else if (values.sortOption === "nameDesc") {
+    result = values.feedbacks.sort((a, b) => {
+      if (a.category!.title!.toLowerCase() > b.category!.title!.toLowerCase())
+        return -1;
+      if (a.category!.title!.toLowerCase() < b.category!.title!.toLowerCase())
+        return 1;
+      return 0;
+    });
+  }
+
+  return result;
+}

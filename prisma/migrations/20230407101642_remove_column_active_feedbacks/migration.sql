cdfd257b734053cee3cac21aecf3cc843b87a12c
e10ne <etiennevanhalteren@hotmail.com>
2023-04-07 12:19:58 +0200

    Remove unused column from database

---
+++ prisma/migrations/20230407101642_remove_column_active_feedbacks/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `active_feedbacks` on the `categories` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `categories` DROP COLUMN `active_feedbacks`;

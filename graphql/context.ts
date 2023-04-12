23b9d416c9519afd881b9793d8640d0d398cb864
e10ne <etiennevanhalteren@hotmail.com>
2023-04-12 13:41:35 +0200

    Add request and response (with correct types) to context

--- graphql/context.ts
+++ graphql/context.ts
@@ -1,13 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../lib/prisma";
 
 export type Context = {
   prisma: PrismaClient;
+  req: NextApiRequest;
+  res: NextApiResponse;
 };
 
-//  req, res
-export async function createContext({}): Promise<Context> {
+export async function createContext(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<Context> {
   return {
     prisma,
+    req,
+    res,
   };
 }

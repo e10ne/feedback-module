9adaf93b01769c17187a6506f022c210a93c581a
e10ne <etiennevanhalteren@hotmail.com>
2023-04-26 09:10:24 +0200

    Add backend logic for login, logout and get current user

--- graphql/types/User.ts
+++ graphql/types/User.ts
@@ -1,5 +1,8 @@
-import { mutationField, nonNull, objectType } from "nexus";
+import { mutationField, nonNull, objectType, queryField } from "nexus";
 import argon2 from "argon2";
+import * as jwt from "jsonwebtoken";
+import cookie from "cookie";
+import { Token } from "../../lib/types";
 
 export const User = objectType({
   name: "User",
@@ -9,8 +12,19 @@ export const User = objectType({
   },
 });
 
+export const UserResponse = objectType({
+  name: "UserResponse",
+  definition(t) {
+    t.field("user", {
+      type: User,
+    });
+    t.string("errorMessage");
+  },
+});
+
 export const Login = mutationField("login", {
-  type: User,
+  type: UserResponse,
+  description: "Login",
   args: {
     name: nonNull("String"),
     password: nonNull("String"),
@@ -22,11 +36,98 @@ export const Login = mutationField("login", {
       },
     });
 
-    if (user) {
-      const valid = await argon2.verify(user.password, args.password);
-      if (valid) return user;
+    if (!user) {
+      return {
+        errorMessage: "Naam/Wachtwoord onjuist",
+      };
     }
 
-    return null;
+    const valid = await argon2.verify(user.password, args.password);
+
+    if (!valid) {
+      return {
+        errorMessage: "Naam/Wachtwoord onjuist",
+      };
+    }
+
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_TOKEN_SECRET as string,
+      { expiresIn: "12h" }
+    );
+
+    ctx.res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("token", token, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 12, // 12 hours
+        path: "/",
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: "strict",
+      })
+    );
+
+    return {
+      user,
+    };
+  },
+});
+
+export const Logout = mutationField("logout", {
+  type: "Boolean",
+  async resolve(_src, _args, ctx) {
+    ctx.res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("token", "", {
+        httpOnly: true,
+        expires: new Date(0),
+        path: "/",
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: "strict",
+      })
+    );
+
+    return true;
+  },
+});
+
+export const Me = queryField("me", {
+  type: User,
+  async resolve(_src, _args, ctx) {
+    const userCookie = ctx.req.cookies["token"];
+
+    if (!userCookie) return null;
+
+    let decodedJwt;
+
+    try {
+      decodedJwt = jwt.verify(
+        userCookie,
+        process.env.JWT_TOKEN_SECRET as string
+      ) as Token;
+    } catch (err) {
+      // cookie has been tampered with
+      ctx.res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("token", "", {
+          httpOnly: true,
+          expires: new Date(0),
+          path: "/",
+          secure: process.env.NODE_ENV !== "development",
+          sameSite: "strict",
+        })
+      );
+      return null;
+    }
+
+    if (!decodedJwt.userId) return null;
+
+    const user = await ctx.prisma.user.findUnique({
+      where: {
+        id: decodedJwt.userId,
+      },
+    });
+
+    return user;
   },
 });

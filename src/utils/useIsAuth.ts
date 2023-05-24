976e7fd5d3f85544daaff5378be30f896b622a76
e10ne <etiennevanhalteren@hotmail.com>
2023-05-24 14:09:40 +0200

    Create auth file again

---
+++ src/utils/useIsAuth.ts
@@ -0,0 +1,29 @@
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useMeQuery } from "../../graphql/generated/graphql";
+import { useRouter } from "next/router";
+
+export const useIsAuth = (setIsLoading: Dispatch<SetStateAction<boolean>>) => {
+  const [{ fetching, data }] = useMeQuery();
+  const [hasCalled, setHasCalled] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (hasCalled) return;
+
+    if (!fetching && !data?.me?.username) {
+      setHasCalled(true);
+      router.replace("/login");
+    } else if (
+      !fetching &&
+      data?.me?.username !== "admin" &&
+      router.pathname === "/admin"
+    ) {
+      setHasCalled(true);
+      router.replace("/");
+    } else if (!fetching && data?.me?.username) {
+      setIsLoading(false);
+      setHasCalled(true);
+      console.log("user: ", data.me.username);
+    }
+  }, [data, fetching]);
+};

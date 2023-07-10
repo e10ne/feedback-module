81c18ba21614e7ac5d55086df3d7cb531fe9360f
e10ne <etiennevanhalteren@hotmail.com>
2023-07-10 12:06:29 +0200

    Update text index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Text } from "@chakra-ui/react";
+import { Button, Flex, Stack, Text } from "@chakra-ui/react";
 import Layout from "../components/layout/Layout";
 import NextLink from "next/link";
 import { withUrqlClient } from "next-urql";
@@ -7,15 +7,24 @@ import { createUrqlClient } from "../../lib/createUrqlClient";
 const Index: React.FC<{}> = ({}) => {
   return (
     <Layout>
-      <Text color="text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </Text>
+      <Stack color="text">
+        <Text>Welkom op de feedback module van Spectrum Multimedia & IT!</Text>
+        <Text>
+          Bij Spectrum Multimedia & IT hechten we veel waarde aan je mening en
+          streven we ernaar om een prettige leer-/werkomgeving te leveren die
+          aansluit bij jouw behoeften. We vinden dat je feedback essentieel is
+          om jouw ervaring bij Spectrum Multimedia & IT te verbeteren waar nodig
+          is.
+        </Text>
+        <Text>
+          Dit platform is speciaal ontworpen voor jou, onze geweldige mediënten,
+          om je gedachten, suggesties en ervaringen met ons te delen. We horen
+          graag over je interacties met ons team, de kwaliteit van onze
+          diensten, en eventuele ideeën die je hebt ter verbetering. Je feedback
+          stelt ons in staat om voortdurend onze werkwijze te verfijnen en te
+          ontwikkelen om aan jouw verwachtingen te voldoen.
+        </Text>
+      </Stack>
       <Flex
         justify={"center"}
         mt={"20"}

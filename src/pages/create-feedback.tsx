9bbec21a7aed8ad74c8683b47c641359fe272006
e10ne <etiennevanhalteren@hotmail.com>
2023-07-11 11:08:56 +0200

    Add styling to text

--- src/pages/create-feedback.tsx
+++ src/pages/create-feedback.tsx
@@ -42,17 +42,19 @@ const CreateFeedback: React.FC<{}> = ({}) => {
             Feedback doorgeven
           </Heading>
           <Text
-            mt={"6"}
-            fontSize={"lg"}
+            mt={"10"}
+            fontSize={"20"}
+            fontFamily={"Open Sans"}
           >
             Of je nu lof, opbouwende kritiek of specifieke verzoeken hebt, we
             moedigen je aan om deze pagina te gebruiken om je gedachten open en
             eerlijk te uiten. Je feedback wordt met de grootste aandacht
             behandeld en speelt een cruciale rol in het vormgeven van de
-            toekomst van Spectrum Multimedia & IT. Vul het formulier zo volledig
-            mogelijk in. Het invullen is anoniem, maar wanneer je feedback
-            specifiek gericht is op jouw behoeften alleen, vragen we je om je
-            naam erbij te vermelden.
+            toekomst van Spectrum Multimedia & IT.
+            <br />
+            Vul het formulier zo volledig mogelijk in. Het invullen is anoniem,
+            maar wanneer je feedback specifiek gericht is op jouw behoeften
+            alleen, vragen we je om je naam erbij te vermelden.
           </Text>
           <Formik
             validateOnChange={false}
@@ -84,6 +86,8 @@ const CreateFeedback: React.FC<{}> = ({}) => {
                   mt={"10"}
                   flexDir={"column"}
                   gap={"4"}
+                  mb={"4"}
+                  color={"#252B42"}
                 >
                   <InputText
                     label="Titel*"
